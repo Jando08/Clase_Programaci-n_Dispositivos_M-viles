9ffd2fabf11fc4386e31e1faf26ec08a4c55cde3
Jando08 <alejandroleyva911@gmail.com>
2026-09-18 08:48:58 -0700

    feat: add examples for array type inference and readonly arrays in TypeScript

--- ts/hola.ts
+++ ts/hola.ts
@@ -58,3 +58,38 @@ if(typeof w === 'object' && w !== null) {
   (w as { runANonExistentMethod: Function }).runANonExistentMethod(); 
 } 
 // Although we have to cast multiple times we can do a check in the if to secure our type and have a safer casting
+
+
+const names: string[] = [];
+names.push("Dylan"); // no error
+// names.push(3); // Error: Argument of type 'number' is not assignable to parameter of type 'string'.
+
+const names1: readonly string[] = ["Dylan"];
+names.push("Jack"); // Error: Property 'push' does not exist on type 'readonly string[]'.
+// try removing the readonly modifier and see if it works?
+
+const numbers = [1, 2, 3]; // inferred to type number[]
+numbers.push(4); // no error
+// comment line below out to see the successful assignment 
+// numbers.push("2"); // Error: Argument of type 'string' is not assignable to parameter of type 'number'.
+let head: number = numbers[2]; // no error
+console.log(head);
+
+// define our tuple
+let ourTuple: [number, boolean, string];
+
+// initialize correctly
+ourTuple = [5, false, 'Coding God was here'];
+
+console.log(ourTuple);
+
+
+// Try playing around with modifying properties and adding ones to see what happens
+const car: { type: string, model: string, year: number, pais: string } = {
+  type: "Toyota",
+  model: "Corolla",
+  year: 2009,
+  pais: "Mexico"
+};
+
+console.log(car);
